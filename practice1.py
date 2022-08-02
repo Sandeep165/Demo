d273eb90cd5551a2c919b8473b2c1471209b7ba8
Sandeep M Waghmare <sandeepwaghmare1999@gmail.com>
2022-08-02 21:29:24 +0530

    practice set1

---
+++ practice1.py
@@ -0,0 +1,99 @@
+''''
+
+Web design :- Django/flask
+
+numeric datatypes :- int,float,complex
+textual datatype :- string
+Sequence datatype :- list,tuple,range
+Mapping datatype :- dict{key:value}
+set datatype:- set, frozenset = providing security to data
+
+username and pswrd:- 
+harry = {"harry@yahoo.com","Happy@206"}
+harry[0] = 
+harry[1] = 
+forzetset(harry)
+'''
+
+'''
+list = [data elements]
+tuple = (data elements )
+set = { data elements}
+dict = {key:value}
+
+BODMAS Rule
+list>tuple>dict>set
+
+
+name = "15"
+'''
+# type :- 
+name = "True"
+new_list = []
+new_list1 = [10]
+new_set = {}    # empty {}  dict?   dict>set
+new_set2 = {"harry"}
+tuple1 = ()
+tuple2 = (10,)  # tuple = (single value)   ->type of the data value ? sol :- add comma
+new_tuple = ((10))
+country = ["India","USA"]
+print(type(new_tuple))
+
+'''
+Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.
+'''
+
+# string   father's name is  Tim
+# name = 'father's name is  Tim'
+f_name = "Harry"
+l_name = ''' 
+Respected Sir/Mam,
+
+        My self XYZ........
+'''
+print(l_name)
+'''
+
+
+sample input :- 4+8
+sample output :- 12
+
+sample input :- 5+0
+sample output :- 5
+
+10-12 hidden test cases
+
+num = int(input(" ")) 48
+result = str(num)   "48"
+lst = []
+for i in result: 
+      lst.append(i)
+      sum(lst)
+
+
+print(sum)
+
+
+
+
+ax**2 + bx + c =0
+3x**2 + 6x + 14 = 0
+a = 3
+b = 6
+c = 14
+x,y
+
+formula method :- {-b +- x}/2a
+x = (b**2-4ac)**0.5
+
+
+10-12 lines
+input 
+op
+
+know how to read the problem
+geting the logic
+'''
